cc6e1cee4eb6dba068c9766bbd54716ce174a231
Jeromy <jeromyj@gmail.com>
2016-01-10 14:28:58 -0800

    basic program

---
+++ dsq.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	ds "github.com/ipfs/go-ipfs/Godeps/_workspace/src/github.com/ipfs/go-datastore"
+	"github.com/ipfs/go-ipfs/repo/fsrepo"
+)
+
+func main() {
+	bkp, err := fsrepo.BestKnownPath()
+	if err != nil {
+		panic(err)
+	}
+
+	r, err := fsrepo.Open(bkp)
+	if err != nil {
+		panic(err)
+	}
+
+	k := ds.NewKey(os.Args[1])
+
+	val, err := r.Datastore().Get(k)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	fmt.Println(val)
+}
